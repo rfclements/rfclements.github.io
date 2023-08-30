3fe9e6993d0a0e6d67e0dc86b13fdd3288be364c
rfclements <143262771+rfclements@users.noreply.github.com>
2023-08-30 15:00:40 +0100

    Update portfolio-slideshow.js
    Signed-off-by: rfclements <143262771+rfclements@users.noreply.github.com>

--- slideshow/portfolio-slideshow.js
+++ slideshow/portfolio-slideshow.js
@@ -1,14 +1,6 @@
 let slideIndex = 0;
 showSlides();
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
