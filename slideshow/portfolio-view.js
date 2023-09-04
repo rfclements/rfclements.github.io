dbce6075b67506834f7acd1809a09f4ce21b67af
rfclements <143262771+rfclements@users.noreply.github.com>
2023-09-04 20:36:58 +0100

    Update portfolio-view.js
    Signed-off-by: rfclements <143262771+rfclements@users.noreply.github.com>

--- slideshow/portfolio-view.js
+++ slideshow/portfolio-view.js
@@ -1,8 +1,14 @@
-$(".images img").click(function(){
-  $("#full-image").attr("src", $(this).attr("src"));
-  $('#image-viewer').show();
-});
-
-$("#image-viewer .close").click(function(){
-  $('#image-viewer').hide();
+$('img[data-enlargable]').addClass('img-enlargable').click(function(){
+    var src = $(this).attr('src');
+    $('<div>').css({
+        background: 'RGBA(0,0,0,.5) url('+src+') no-repeat center',
+        backgroundSize: 'contain',
+        width:'100%', height:'100%',
+        position:'fixed',
+        zIndex:'10000',
+        top:'0', left:'0',
+        cursor: 'zoom-out'
+    }).click(function(){
+        $(this).remove();
+    }).appendTo('body');
 });
